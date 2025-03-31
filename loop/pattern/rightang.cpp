82674a0b75f0112dd221a055cce10546ec3af643
sayali singh <sayalibaghel6@gmail.com>
2025-03-31 22:46:37 +0530

    mew

--- loop/pattern/rightang.cpp
+++ loop/pattern/rightang.cpp
@@ -4,18 +4,34 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++)
-    {
-        for (int c = 4; c >= i; c--)
-        {
+    int k;
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         for (int c = 4; c >= i; c--)
+//         {
 
-            cout << " ";
-        }
-        for (int s = 1; s <= i; s++)
-        {
-            cout << i;
+//             cout << " ";
+//         }
+//         for (int s = 1; s <= i; s++)
+//         {
+//             cout << i;
             
-        }
-        cout << "\n";
+//         }
+//         cout << "\n";
+//     }
+// }
+for (int i = 1; i <= 5; i++)
+{
+    for (int c = 4; c >= i; c--)
+    {
+
+        cout << " ";
     }
+    for (int s = 1; s <= i; s++)
+    {
+        cout << s;
+        
+    }
+    cout << "\n";
+}
 }
