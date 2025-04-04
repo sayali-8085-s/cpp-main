f408fbcfa8135fc2234f1b218cf1728a69a7356e
sayali singh <sayalibaghel6@gmail.com>
2025-04-04 22:23:39 +0530

    new

---
+++ practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a number";
+    cin>>a;
+    int b;
+    cout<<"enter a second number";
+    cin>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<a<<b;
+}
+
+
+
