b2ea84cd76edfbd994db954577bc28657039b641
sayali singh <sayalibaghel6@gmail.com>
2025-04-05 22:23:58 +0530

    new

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a;
-    cout<<"enter a number";
-    cin>>a;
-    int b;
-    cout<<"enter a second number";
-    cin>>b;
-    a=a+b;
-    b=a-b;
-    a=a-b;
-    cout<<a<<b;
+
+int n ,count;
+cout<<"enter a num";
+cin>>n;
+count=0;
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+count++;
+    }
+
 }
-
-
-
+if(count==2){
+    cout<<"prime";
+}
+else{
+    cout<<"not";
+}
+}
